4d771df401dd260b8582637da520615f5c20bf4a
Liam Shaw <liam.philip.shaw@gmail.com>
2023-05-12 13:07:23 +0100

    sorted out altair plot ordering

--- scripts/plot_output_dists_altair.py
+++ scripts/plot_output_dists_altair.py
@@ -2,8 +2,6 @@ import altair as alt
 import csv
 import json
 import pandas as pd
-# from vega_datasets import data
-
 
 def my_theme():
   return {
@@ -15,13 +13,15 @@ def my_theme():
 alt.themes.register('my_theme', my_theme)
 alt.themes.enable('my_theme')
 
+interpolate_option = "step"
+
 
 # Open the CSV file for reading
 GENE = "CTX-M-15"
+# BELOW ONLY NEEDED FOR NOT YET CONVERTED CSV TO JSON
 with open(GENE+'.output_dists.csv', newline='') as csvfile:
     # Read the CSV file into a dictionary
     reader = csv.DictReader(csvfile)
-
     # Convert the dictionary to a list of rows
     rows = list(reader)
 
@@ -31,18 +31,15 @@ json_str = json.dumps(rows)
 # Print the JSON string
 with open(GENE+'.output_dists.json', 'w') as f:
         f.write(json_str)
+# ^^^ ONLY NEEDED FOR NOT YET CONVERTED CSV TO JSON
+
 
 df = pd.read_json(GENE+'.output_dists.json')
-print(df)
 # Convert the data types of the columns to float if necessary
 df['distup'] = df['dist.up'].astype(float)
 df['distdown'] = df['dist.down'].astype(float)
-# def map_to_categorical_snp(value):
-#     if value <= 7:
-#         return {0: chr(value), 1: chr(value), 2: chr(value), 3: chr(value), 4: chr(value), 5: chr(value), 6: chr(value), 7: chr(value)}
-#     else:
-#         return {">7": value}
 
+# Map SNPs to categories - have >7 as (arbitrary) highest category
 def map_to_categorical_snp(values):
     return_values = [""] * len(values)
     for i,v in enumerate(values):
@@ -52,99 +49,37 @@ def map_to_categorical_snp(values):
             return_values[i] = ">7"
     return return_values
 
-#print(list(df['snps']))
 df['snpscategorical'] = map_to_categorical_snp(list(df['snps']))
+
 
 max_dist = max(df['distup'])-10 # to clip the edges where it artifically goes to zero
 
 
-
-cdf4_up = alt.Chart(df).transform_window(
-    ecdf="cume_dist()",
-    sort=[{"field": "distup"}],
-).mark_line(
-    interpolate="step-before", clip=True
-).encode(
-    x=alt.X("distup:Q",
-        scale=alt.Scale(reverse=True, domain=[0,max_dist])),
-    y=alt.Y('ecdf:Q', scale=alt.Scale(reverse=True)),
-    color='snpscategorical:N'
-)
-cdf4_down = alt.Chart(df).transform_window(
-    ecdf="cume_dist()",
-    sort=[{"field": "distdown"}],
-).mark_line(
-    interpolate="step-before", clip=True
-).encode(
-    x=alt.X("distdown:Q", 
-        scale=alt.Scale(domain=[0, max_dist])),
-    y=alt.Y('ecdf:Q', scale=alt.Scale(reverse=True)),
-    color='snpscategorical:N'
-)
-cdf4 = cdf4_up | cdf4_down
-cdf4.save(GENE+'-cdf.html')
-
-
-# Try adding dropdown selection
-options = ['0', '1', '2', '3', '4', '5', '6', '7', '>7']
+# For dropdown selection for SNP category of comparison
+options = sorted(set(df['snpscategorical']))
 labels = [option + ' ' for option in options]
+# Sort labels in the same order
+labels_sorted = [label for _, label in sorted(zip(options, labels))]
 
 input_dropdown = alt.binding_radio(
-    # Add the empty selection which shows all when clicked
     options=options + [None],
-    labels=labels + ['All'],
+    labels=labels_sorted + ['All'],
     name='SNPs: '
 )
+
 selection = alt.selection_point(
     fields=['snpscategorical'],
-    bind=input_dropdown
+    bind=input_dropdown,
+    #empty='all'
 )
-# input_dropdown = alt.binding_select(options=["0","1","2","3","4","5","6","7",">7"], 
-#     name='SNPs ')
-# selection = alt.selection_point(fields=['snpscategorical'], bind=input_dropdown)
+
 color = alt.condition(
-    selection,
+    selection ,# & (alt.datum.snpscategorical != 'None'),
     alt.Color('snpscategorical:N').legend(None),
     alt.value('lightgray')
 )
 
-cdf5_up = alt.Chart(df).transform_window(
-    ecdf="cume_dist()",
-    sort=[{"field": "distup"}],
-).mark_line(
-    interpolate="step-before", clip=True
-).encode(
-    x=alt.X("distup:Q",
-        scale=alt.Scale(reverse=True, domain=[0,max_dist])),
-    y=alt.Y('ecdf:Q', scale=alt.Scale(reverse=True)),
-    color=color
-)#.add_params(
-  #  selection
-#).transform_filter(
-#    selection
-#)
-
-
-
-cdf5_down = alt.Chart(df).transform_window(
-    ecdf="cume_dist()",
-    sort=[{"field": "distdown"}],
-).mark_line(
-    interpolate="step-before", clip=True
-).encode(
-    x=alt.X("distdown:Q", 
-        scale=alt.Scale(domain=[0, max_dist])),
-    y=alt.Y('ecdf:Q', scale=alt.Scale(reverse=True)),
-    #color='snpscategorical:N'
-    color=color
-)#.add_params(
- #   selection
-#).transform_filter(
-#    selection
-#)
-
-
-
+# Produce legend
 legend = alt.Chart(df).mark_point().encode(
     alt.Y('snpscategorical:N').axis(orient='right'),
     color=color
@@ -152,92 +87,84 @@ legend = alt.Chart(df).mark_point().encode(
     selection
 )
 
-cdf5_up_selected = alt.Chart(df).transform_filter(
+
+def ecdf(values):
+    '''returns the ecdf for values'''
+    v_sorted = [0]+sorted(values)
+    return pd.DataFrame({'ecdf':[x/len(values) for x in range(0, len(values)+1)],
+                    'dist':v_sorted})
+
+# Create ecdf dataframes
+plot_df_up = df.groupby('snpscategorical').apply(lambda x: ecdf(x['distup'])).reset_index(level=1, drop=True)
+plot_df_up = plot_df_up.assign(snpscategorical=plot_df_up.index)
+plot_df_down = df.groupby('snpscategorical').apply(lambda x: ecdf(x['distdown'])).reset_index(level=1, drop=True)
+plot_df_down = plot_df_down.assign(snpscategorical=plot_df_down.index)
+
+# The strategy for selection is that 
+# we use an unselected one as a background layer 
+# and put the selected one on top
+
+# Upstream plots
+cdf_plot_up_selected = alt.Chart(plot_df_up).transform_filter(
     selection
-).transform_window(
-    ecdf="cume_dist()",
-    sort=[{"field": "distup"}],
 ).mark_line(
-    interpolate="step-before",
+    interpolate=interpolate_option,
     clip=True,
     strokeWidth=2,
     opacity=1,
 ).encode(
-    x=alt.X("distup:Q",
+    x=alt.X("dist:Q",
             scale=alt.Scale(reverse=True, domain=[0, max_dist])),
     y=alt.Y('ecdf:Q', scale=alt.Scale(reverse=True, domain=[0,1])),
     color=alt.Color('snpscategorical:N').legend(None),
 )
-
-cdf5_up_unselected = alt.Chart(df).transform_filter(
-    ~selection
-).transform_window(
-    ecdf="cume_dist()",
-    sort=[{"field": "distup"}],
-).mark_line(
-    interpolate="step-before",
+cdf_plot_up_unselected = alt.Chart(plot_df_up).mark_line(
+    interpolate=interpolate_option,
     clip=True,
     strokeWidth=2,
-    opacity=0.4,
+    opacity=0.8,
 ).encode(
-    x=alt.X("distup:Q",
+    x=alt.X("dist:Q",
             scale=alt.Scale(reverse=True, domain=[0, max_dist])),
-    y=alt.Y('ecdf:Q', scale=alt.Scale(reverse=True, domain=[0, 1])),
-     color=alt.Color('snpscategorical:N', scale=None#, 
-#        sort=alt.EncodingSortField('snpscategorical')),
-)
+    y=alt.Y('ecdf:Q', scale=alt.Scale(reverse=True, domain=[0,1])),
+    color=alt.Color('snpscategorical:N', scale=None, 
+    sort=alt.EncodingSortField('snpscategorical')),
+    tooltip=[alt.Tooltip("dist:Q", title="")]
 )
 
-cdf5_down_selected = alt.Chart(df).transform_filter(
+
+# Downstream plots
+cdf_plot_down_selected = alt.Chart(plot_df_down).transform_filter(
     selection
-).transform_window(
-    ecdf="cume_dist()",
-    sort=[{"field": "distdown"}],
 ).mark_line(
-    interpolate="step-before",
+    interpolate=interpolate_option,
     clip=True,
     strokeWidth=2,
     opacity=1,
 ).encode(
-    x=alt.X("distdown:Q",
+    x=alt.X("dist:Q",
             scale=alt.Scale(domain=[0, max_dist])),
     y=alt.Y('ecdf:Q', scale=alt.Scale(reverse=True, domain=[0, 1])),
     color=alt.Color('snpscategorical:N').legend(None),
 )
-
-cdf5_down_unselected = alt.Chart(df).transform_filter(
-    ~selection
-).transform_window(
-    ecdf="cume_dist()",
-    sort=[{"field": "distdown"}],
-).mark_line(
-    interpolate="step-before",
+cdf_plot_down_unselected = alt.Chart(plot_df_down).mark_line(
+    interpolate=interpolate_option,
     clip=True,
     strokeWidth=2,
-    opacity=0.4,
+    opacity=0.8,
 ).encode(
-    x=alt.X("distdown:Q",
+    x=alt.X("dist:Q",
             scale=alt.Scale(domain=[0, max_dist])),
     y=alt.Y('ecdf:Q', scale=alt.Scale(reverse=True, domain=[0,1])),
-     color=alt.Color('snpscategorical:N', scale=None#, 
-#        sort=alt.EncodingSortField('snpscategorical')),
+    color=alt.Color('snpscategorical:N', scale=None, 
+    sort=alt.EncodingSortField('snpscategorical')),
+    tooltip=[alt.Tooltip("dist:Q", title="")]
 )
-)
 
-cdf5_up = (cdf5_up_unselected+cdf5_up_selected ).add_selection(selection)
-cdf5_down = (cdf5_down_unselected+cdf5_down_selected ).add_selection(selection)
+# Combine the plots
+cdf_plot_up = (cdf_plot_up_unselected+cdf_plot_up_selected ).add_params(selection)
+cdf_plot_down = cdf_plot_down_unselected.add_params(selection)+cdf_plot_down_selected 
+cdf_plot_combined = cdf_plot_up | cdf_plot_down | legend
 
-
-
-# alt.layer(
-#   cdf5_up.add_params(selection),
-#   cdf5_up.transform_filter(selection).encode( f"toDate(datum.snpscategorical) == {selection.snpscategorical}")
-# )
-
-cdf5_combined = cdf5_up | cdf5_down | legend
-cdf5_combined.save(GENE+'-cdf-selector.html')
-
-cdf5_down.save(GENE+'-cdf-selector-down.html')
-
-
-
+# Save them
+cdf_plot_combined.save(GENE+'-cdf-selector.html')
