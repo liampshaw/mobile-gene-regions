58823f7afda19e2f1a20a01cd516a4a5a834bfa2
Liam Shaw <liam.philip.shaw@gmail.com>
2023-08-07 10:22:45 +0100

    helper text

--- beta-lactamases/analysis/make_figures.sh
+++ beta-lactamases/analysis/make_figures.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
+echo "############################"
+echo "Setting up breakpoint plots..."
+echo "############################"
 ./01_make_breakpoint_plots.sh  
+
+
+
+echo "############################"
+echo "Making CTX-M-65 figure..."
+echo "############################"
 ./02_make_CTX-M-65_figure.sh  
+
+
+echo "############################"
+echo "Making breakpoint AUC plots..."
+echo "############################"
 Rscript 03_make_breakpoint_AUC_plots.R
+
+echo "############################"
+echo "Making annotation/entropy/breakpoint curves..."
+echo "############################"
 ./04_make_entropy_transposase_curves.sh
